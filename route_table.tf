f6332f2fde068dfaa6fc1f2eb89189b81cb78ce7
Yelizaveta Ivanchenko <yelizaveta.ivanchenko@gmx.com>
2022-09-16 12:23:19 +0200

    new vpc update project

--- route_table.tf
+++ route_table.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "public_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gateway.id
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
 
@@ -18,7 +18,7 @@ resource "aws_route_table" "private_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway.id
+    gateway_id = aws_nat_gateway.nat_gateway.id
   }
 
 
@@ -26,4 +26,32 @@ resource "aws_route_table" "private_route_table" {
   tags = {
     Name = "private_route_table"
   }
+}
+
+# ROUTE TABLE ASSOCIATION PUBLIC
+resource "aws_route_table_association" "public_rt_subnet_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_route_table_association" "public_rt_subnet_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_route_table_association" "public_rt_subnet_association_3" {
+  subnet_id      = aws_subnet.public_subnet_3.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# ROUTE TABLE ASSOCIATION PRIVATE
+resource "aws_route_table_association" "private_rt_subnet_association_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+resource "aws_route_table_association" "private_rt_subnet_association_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+resource "aws_route_table_association" "private_rt_subnet_association_3" {
+  subnet_id      = aws_subnet.private_subnet_3.id
+  route_table_id = aws_route_table.private_route_table.id
 }
